e34e19214042a3b493b9ca8172cec262a923e82f
gz-adrian <102628359+gz-adrian@users.noreply.github.com>
2022-06-02 22:00:41 -0700

    fixed

--- ram.js
+++ ram.js
@@ -43,7 +43,7 @@ app.listen(PORT, () => {
     })
 
     // /more here
-    
+
 
     // /more here
 
@@ -66,34 +66,64 @@ app.listen(PORT, () => {
 //     'age': 29,
 //     'birthName': 'Sheya Abraham-Joseph',
 //     'birthLocation': 'London, England'
-//   },
-//   'charlie day': {
-//     'age': 46,
-//     'birthName': 'New York',
-//     'birthLocation': 'New York, USA'
-//   },
-//   'unknown': {
-//     'age': 0,
-//     'birthName': 'unknown.',
-//     'birthLocation': 'unknown'
+
 //   },
 // }
 
-// app.get('/', (request, response) => { // its an event listener. its a Network Request
-//   response.sendFile(__dirname + '/index.html')
-// })
+app.get('/', (request, response) => { // its an event listener. its a Network Request
+      response.sendFile(__dirname + '/index.html')
+    })
 
-// // Create another get Router
-// app.get('/api/:name', (request, response) => {// [/'api'] it was called /:name
-//   const rapperName = request.params.name.toLowerCase() // what name for params.name and /:name'
+// Create another get Router
+app.get('/api/:name', (request, response) => {// [/'api'] it was called /:name
+      const rapperName = request.params.name.toLowerCase() // what name for params.name and /:name'
 
-//   if (rappers[rapperName]) {  // conditional statement
-//     response.json(rappers[rapperName]) // all we do here is respond to savage
-//   } else {
-//     response.json(rappers['unknown'])
-//   }
+      if (rappers[rapperName]) {  // conditional statement
+        response.json(rappers[rapperName]) // all we do here is respond to savage
+      } else {
+        response.json(rappers['unknown'])
+      }
 
-// })
+    })
+  })
+
+  // Create another get Router
+  app.get('/api/:name', (request, response) => {// [/'api'] it was called /:name
+    const rapperName = request.params.name.toLowerCase() // what name for params.name and /:name'
+
+    if (rappers[rapperName]) {  // conditional statement
+      response.json(rappers[rapperName]) // all we do here is respond to savage
+    } else {
+      response.json(rappers['unknown'])
+    }
+
+  })
+})
+
+  // Create another get Router
+  app.get('/api/:name', (request, response) => {// [/'api'] it was called /:name
+    const rapperName = request.params.name.toLowerCase() // what name for params.name and /:name'
+
+    if (rappers[rapperName]) {  // conditional statement
+      response.json(rappers[rapperName]) // all we do here is respond to savage
+    } else {
+      response.json(rappers['unknown'])
+    }
+
+  })
+    })
+
+// Create another get Router
+app.get('/api/:name', (request, response) => {// [/'api'] it was called /:name
+  const rapperName = request.params.name.toLowerCase() // what name for params.name and /:name'
+
+  if (rappers[rapperName]) {  // conditional statement
+    response.json(rappers[rapperName]) // all we do here is respond to savage
+  } else {
+    response.json(rappers['unknown'])
+  }
+
+})
 
 // app.listen(PORT, () => {
 //   console.log(`The server is now running on port ${PORT}! Betta Go catch it!`)
