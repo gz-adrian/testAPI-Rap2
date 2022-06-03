7057af6de1320529528a3371c8353924787ebbc5
gz-adrian <102628359+gz-adrian@users.noreply.github.com>
2022-06-02 22:01:12 -0700

    fixed

--- ram.js
+++ ram.js
@@ -21,6 +21,66 @@ const rappers = { //savage list is the object. send it as a json object
     //     'birthLocation': 'unknown'
     //   },
     // }
+    'age': 29,
+    'birthName': 'Sheya Abraham-Joseph',
+    'birthLocation': 'London, England'
+  },
+  'charlie day': {
+    'age': 46,
+    //     'birthName': 'New York',
+    //     'birthLocation': 'New York, USA'
+    //   },
+    //   'unknown': {
+    //     'age': 0,
+    //     'birthName': 'unknown.',
+    //     'birthLocation': 'unknown'
+    //   },
+    // }
+    'age': 29,
+    'birthName': 'Sheya Abraham-Joseph',
+    'birthLocation': 'London, England'
+  },
+  'charlie day': {
+    'age': 46,
+    //     'birthName': 'New York',
+    //     'birthLocation': 'New York, USA'
+    //   },
+    //   'unknown': {
+    //     'age': 0,
+    //     'birthName': 'unknown.',
+    //     'birthLocation': 'unknown'
+    //   },
+    // }
+    'age': 29,
+    'birthName': 'Sheya Abraham-Joseph',
+    'birthLocation': 'London, England'
+  },
+  'charlie day': {
+    'age': 46,
+    //     'birthName': 'New York',
+    //     'birthLocation': 'New York, USA'
+    //   },
+    //   'unknown': {
+    //     'age': 0,
+    //     'birthName': 'unknown.',
+    //     'birthLocation': 'unknown'
+    //   },
+    // }
+    'age': 29,
+    'birthName': 'Sheya Abraham-Joseph',
+    'birthLocation': 'London, England'
+  },
+  'charlie day': {
+    'age': 46,
+    //     'birthName': 'New York',
+    //     'birthLocation': 'New York, USA'
+    //   },
+    //   'unknown': {
+    //     'age': 0,
+    //     'birthName': 'unknown.',
+    //     'birthLocation': 'unknown'
+    //   },
+    // }
 
     // app.get('/', (request, response) => { // its an event listener. its a Network Request
     //   response.sendFile(__dirname + '/index.html')
@@ -90,43 +150,4 @@ app.get('/api/:name', (request, response) => {// [/'api'] it was called /:name
   // Create another get Router
   app.get('/api/:name', (request, response) => {// [/'api'] it was called /:name
     const rapperName = request.params.name.toLowerCase() // what name for params.name and /:name'
-
-    if (rappers[rapperName]) {  // conditional statement
-      response.json(rappers[rapperName]) // all we do here is respond to savage
-    } else {
-      response.json(rappers['unknown'])
-    }
-
-  })
-})
-
-  // Create another get Router
-  app.get('/api/:name', (request, response) => {// [/'api'] it was called /:name
-    const rapperName = request.params.name.toLowerCase() // what name for params.name and /:name'
-
-    if (rappers[rapperName]) {  // conditional statement
-      response.json(rappers[rapperName]) // all we do here is respond to savage
-    } else {
-      response.json(rappers['unknown'])
-    }
-
-  })
-    })
-
-// Create another get Router
-app.get('/api/:name', (request, response) => {// [/'api'] it was called /:name
-  const rapperName = request.params.name.toLowerCase() // what name for params.name and /:name'
-
-  if (rappers[rapperName]) {  // conditional statement
-    response.json(rappers[rapperName]) // all we do here is respond to savage
-  } else {
-    response.json(rappers['unknown'])
-  }
-
-})
-
-// app.listen(PORT, () => {
-//   console.log(`The server is now running on port ${PORT}! Betta Go catch it!`)
-// })
-
 
