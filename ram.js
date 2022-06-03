1b5c735388e32e8dfc8a2450ca08d42a3baff165
gz-adrian <102628359+gz-adrian@users.noreply.github.com>
2022-06-02 21:59:33 -0700

    fixed

--- ram.js
+++ ram.js
@@ -12,79 +12,91 @@ const rappers = { //savage list is the object. send it as a json object
   },
   'charlie day': {
     'age': 46,
-    'birthName': 'New York',
-    'birthLocation': 'New York, USA'
-  },
-  'unknown': {
-    'age': 0,
-    'birthName': 'unknown.',
-    'birthLocation': 'unknown'
-  },
-}
+    //     'birthName': 'New York',
+    //     'birthLocation': 'New York, USA'
+    //   },
+    //   'unknown': {
+    //     'age': 0,
+    //     'birthName': 'unknown.',
+    //     'birthLocation': 'unknown'
+    //   },
+    // }
 
-app.get('/', (request, response) => { // its an event listener. its a Network Request
-  response.sendFile(__dirname + '/index.html')
-})
+    // app.get('/', (request, response) => { // its an event listener. its a Network Request
+    //   response.sendFile(__dirname + '/index.html')
+    // })
 
-// Create another get Router
-app.get('/api/:name', (request, response) => {// [/'api'] it was called /:name
-  const rapperName = request.params.name.toLowerCase() // what name for params.name and /:name'
+    // Create another get Router
+    app.get('/api/:name', (request, response) => {// [/'api'] it was called /:name
+      const rapperName = request.params.name.toLowerCase() // what name for params.name and /:name'
 
-  if (rappers[rapperName]) {  // conditional statement
-    response.json(rappers[rapperName]) // all we do here is respond to savage
-  } else {
-    response.json(rappers['unknown'])
-  }
+      if (rappers[rapperName]) {  // conditional statement
+        response.json(rappers[rapperName]) // all we do here is respond to savage
+      } else {
+        response.json(rappers['unknown'])
+      }
 
-})
-
-app.listen(PORT, () => {
-  console.log(`The server is now running on port ${PORT}! Betta Go catch it!`)
-})
-
-
-
-// const { response, Router } = require('express') //I think this is a mistake
-const express = require('express')
-const app = express()
-const PORT = 8002
-
-const rappers = { //savage list is the object. send it as a json object
-  '21 savage': {
-    'age': 29,
-    'birthName': 'Sheya Abraham-Joseph',
-    'birthLocation': 'London, England'
-  },
-  'charlie day': {
-    'age': 46,
-    'birthName': 'New York',
-    'birthLocation': 'New York, USA'
-  },
-  'unknown': {
-    'age': 0,
-    'birthName': 'unknown.',
-    'birthLocation': 'unknown'
-  },
-}
-
-app.get('/', (request, response) => { // its an event listener. its a Network Request
-  response.sendFile(__dirname + '/index.html')
-})
-
-// Create another get Router
-app.get('/api/:name', (request, response) => {// [/'api'] it was called /:name
-  const rapperName = request.params.name.toLowerCase() // what name for params.name and /:name'
-
-  if (rappers[rapperName]) {  // conditional statement
-    response.json(rappers[rapperName]) // all we do here is respond to savage
-  } else {
-    response.json(rappers['unknown'])
-  }
-
-})
+    })
 
 app.listen(PORT, () => {
-  console.log(`The server is now running on port ${PORT}! Betta Go catch it!`)
-})
+      console.log(`The server is now running on port ${PORT}! Betta Go catch it!`)
+    })
+
+    // /more here
+    
+
+    // /more here
+
+
+    // /more here
+
+
+    // /more here
+
+
+
+
+// // const { response, Router } = require('express') //I think this is a mistake
+// const express = require('express')
+// const app = express()
+// const PORT = 8002
+
+// const rappers = { //savage list is the object. send it as a json object
+//   '21 savage': {
+//     'age': 29,
+//     'birthName': 'Sheya Abraham-Joseph',
+//     'birthLocation': 'London, England'
+//   },
+//   'charlie day': {
+//     'age': 46,
+//     'birthName': 'New York',
+//     'birthLocation': 'New York, USA'
+//   },
+//   'unknown': {
+//     'age': 0,
+//     'birthName': 'unknown.',
+//     'birthLocation': 'unknown'
+//   },
+// }
+
+// app.get('/', (request, response) => { // its an event listener. its a Network Request
+//   response.sendFile(__dirname + '/index.html')
+// })
+
+// // Create another get Router
+// app.get('/api/:name', (request, response) => {// [/'api'] it was called /:name
+//   const rapperName = request.params.name.toLowerCase() // what name for params.name and /:name'
+
+//   if (rappers[rapperName]) {  // conditional statement
+//     response.json(rappers[rapperName]) // all we do here is respond to savage
+//   } else {
+//     response.json(rappers['unknown'])
+//   }
+
+// })
+
+// app.listen(PORT, () => {
+//   console.log(`The server is now running on port ${PORT}! Betta Go catch it!`)
+// })
 
 
